0b9d058493e4c9af1c6014c1ffdd226bcdfade84
Adam Michel <adam@amichel.me>
2014-08-12 23:41:54 -0400

    Fix ApiIntegration saving within ActiveAdmin.

--- app/models/api_integration.rb
+++ app/models/api_integration.rb
@@ -1,4 +1,6 @@
 class ApiIntegration < ActiveRecord::Base
+    attr_accessible :provider, :app, :key, :secret, :oauth_token, :oauth_secret, :location_settings_id
+
     belongs_to :location_settings
     validates_uniqueness_of :provider, :message => "An integration already exists for this provider."
 
