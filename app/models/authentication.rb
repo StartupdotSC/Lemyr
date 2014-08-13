9b07429dcc6cce88f91010e47d51b07b649b55e1
Adam Michel <adam@amichel.me>
2014-08-13 01:09:20 -0400

    Add attr_accessors to Authentication model.

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,11 +1,13 @@
 class Authentication < ActiveRecord::Base
-    belongs_to :user
+  attr_accessible :provider, :uid, :token, :secret, :name
 
-    def update_omniauth(omniauth)
-        self.update_attributes(:provider => omniauth['provider'], 
-                          :uid => omniauth['uid'], 
-                          :token => omniauth['credentials']['token'], 
-                          :secret => omniauth['credentials']['secret'], 
-                          :name => omniauth['info']['nickname'])
-    end
+  belongs_to :user
+
+  def update_omniauth(omniauth)
+    self.update_attributes(:provider => omniauth['provider'],
+      :uid => omniauth['uid'],
+      :token => omniauth['credentials']['token'],
+      :secret => omniauth['credentials']['secret'],
+      :name => omniauth['info']['nickname'])
+  end
 end
