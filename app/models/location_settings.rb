bef888b7242c4d64b5f61f3ec7fb4502ca1b5a0f
Adam Michel <adam@amichel.me>
2014-09-08 01:59:24 -0400

    Use S3 domain style URL, fixes avatar images.

--- app/models/location_settings.rb
+++ app/models/location_settings.rb
@@ -77,6 +77,7 @@ class LocationSettings < ActiveRecord::Base
 
     if !api.nil?
       Paperclip::Attachment.default_options[:storage] = :s3
+      Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
       Paperclip::Attachment.default_options[:s3_protocol] = 'https'
       Paperclip::Attachment.default_options[:s3_credentials] = {
         :bucket => api.app, #ENV['AWS_BUCKET'],
