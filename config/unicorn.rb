31db691911679596a8a1ce3ad29a58701de3a12a
Adam Michel <adam@amichel.me>
2014-09-07 20:51:01 -0400

    Update Unicorn config to fix deployment issues.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,23 @@
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-listen "unix:tmp/sockets/unicorn.socket", :backlog => 64
+user 'deploy', 'deploy'
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 30
 preload_app true
-pid "tmp/pids/unicorn.pid"
 
-# Production specific settings
-if rails_env == "production"
-  working_directory "/home/deploy/apps/lemyr/current"
+working_directory "/home/deploy/apps/lemyr/current"
+shared_path = "/home/deploy/apps/lemyr/shared"
 
-  user 'deploy', 'deploy'
-  shared_path = "/home/deploy/apps/lemyr/shared"
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
 
-  stderr_path "#{shared_path}/log/unicorn.stderr.log"
-  stdout_path "#{shared_path}/log/unicorn.stdout.log"
-end
+pid "#{shared_path}/tmp/pids/unicorn.pid"
+listen "unix:#{shared_path}/tmp/sockets/unicorn.socket", :backlog => 64
 
 before_fork do |server, worker|
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
   end
 
-  old_pid = "tmp/pids/unicorn.pid.oldbin"
+  old_pid = "#{shared_path}/tmp/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
